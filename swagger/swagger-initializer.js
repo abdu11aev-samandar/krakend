bbeaeaa056d0488b0a166d89e61b5b4f0807de01
s_abdulldyev <S.Abdulldyev@uzinfocom.uz>
2024-11-11 11:29:43 +0500

    Added local swagger ui

---
+++ swagger/swagger-initializer.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  window.ui = SwaggerUIBundle({
+    urls: [
+      {url: "/crm/docs/api-docs.json", name: "CRM API"},
+      {url: "/call-center/docs/api-docs.json", name: "Call Center API"},
+      {url: "/gateway/docs/api-docs.json", name: "Gateway API"},
+      {url: "/chat-api/docs/api-docs.json", name: "Chat API"},
+    ],
+    "urls.primaryName": "API Two",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  });
+};
